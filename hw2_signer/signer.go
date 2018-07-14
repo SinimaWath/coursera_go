c1f4e845eee24e132cf2b23f562e9116f2a4c2d5
Sinimawath <sinimawath@gmail.com>
2018-07-14 16:24:32 +0300

    Passed all test

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 func worker(wg *sync.WaitGroup, j job, in, out chan interface{}) {
@@ -27,60 +29,95 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mutex := &sync.Mutex{}
 	for chVal := range in {
-		var strVal string
-		switch val := chVal.(type) {
-		case int:
-			strVal = strconv.Itoa(val)
-		case string:
-			strVal = val
-		}
-		res := DataSignerCrc32(strVal) + "~" + DataSignerCrc32(DataSignerMd5(strVal))
-		fmt.Println("SingleHash data:", strVal)
-		fmt.Println("SingleHash Result: ", res)
-		out <- res
+		wg.Add(1)
+		go func(chVal interface{}, wg *sync.WaitGroup) {
+			defer wg.Done()
+
+			var strVal string
+			switch val := chVal.(type) {
+			case int:
+				strVal = strconv.Itoa(val)
+			case string:
+				strVal = val
+			}
+
+			ch1 := make(chan string, 1)
+			ch2 := make(chan string, 1)
+			worker := func(strVal string, out chan<- string) {
+				out <- DataSignerCrc32(strVal)
+			}
+
+			go worker(strVal, ch1)
+
+			mutex.Lock()
+			md5Val := DataSignerMd5(strVal)
+			mutex.Unlock()
+
+			go worker(md5Val, ch2)
+
+			first := <-ch1
+			fmt.Println("SH first: ", first)
+			second := <-ch2
+			fmt.Println("Sh second: ", second)
+
+			out <- (first + "~" + second)
+		}(chVal, wg)
 	}
+	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 	for chVal := range in {
-		val, ok := chVal.(string)
-		var res string
-		if !ok {
-			panic("Can' not use val as not string")
-		}
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, chVal interface{}) {
+			defer wg.Done()
+			val, ok := chVal.(string)
+			var res string
+			if !ok {
+				panic("Can't use val as not string")
+			}
 
-		for i := 0; i < 6; i++ {
-			strI := strconv.Itoa(i)
-			res += DataSignerCrc32(strI + val)
-		}
-		fmt.Println("MultiHash data: ", chVal)
-		fmt.Println("MultiHash Result: ", res)
-		out <- res
+			chSlice := make([]chan string, 6, 6)
+			for i := 0; i < 6; i++ {
+				chSlice[i] = make(chan string, 1)
+				go func(i int, val string, ch chan<- string) {
+					strI := strconv.Itoa(i)
+					ch <- DataSignerCrc32(strI + val)
+				}(i, val, chSlice[i])
+			}
+
+			for _, val := range chSlice {
+				res += <-val
+			}
+			fmt.Println("MultiHash data: ", chVal)
+			fmt.Println("MultiHash Result: ", res)
+			out <- res
+		}(wg, chVal)
 	}
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
-	var res string
+	var strVals []string
 	for chVal := range in {
 		val, ok := chVal.(string)
 		if !ok {
 			panic("Can' not use val as not string")
 		}
-		res += val
-		res += "_"
+		strVals = append(strVals, val)
 	}
 
-	if strings.HasSuffix(res, "_") {
-		out <- strings.TrimSuffix(res, "_")
-	} else {
-		out <- res
-	}
+	sort.Strings(strVals)
+	out <- strings.Join(strVals, "_")
 }
 
 func main() {
 
-	inputData := []int{0, 1}
+	inputData := []int{0, 1, 1, 2, 3, 5}
 
 	hashSignJobs := []job{
 		job(func(in, out chan interface{}) {
@@ -101,6 +138,9 @@ func main() {
 		}),
 	}
 
+	start := time.Now()
 	ExecutePipeline(hashSignJobs...)
+	end := time.Since(start)
+	fmt.Println("Time passed from: ", end)
 
 }
